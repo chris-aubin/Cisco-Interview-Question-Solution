5de3dd398c9e3ec2f6e753e651240856bdf4503c
Chris Aubin <69503979+chris-aubin@users.noreply.github.com>
2022-01-23 20:11:13 +0200

    Check for E3 (parent with more than 2 children)

--- binary_tree.js
+++ binary_tree.js
@@ -11,5 +11,21 @@ let childrenArray = pairsArrays.map(i => i[1]);
 // Check for E2 (duplicate (Parent, Child) pairs)
 let setPairsStrings = new Set(pairsStrings);
 if (setPairsStrings.size !== pairsStrings.length) {
-  return 'E2'
+  return 'E2';
 }
+
+// Count how many time each parent occurs
+let parentsCounts = parentsArray.reduce((prev, curr) => {
+  prev[curr] = (prev[curr] || 0) + 1
+  return prev
+}, {})
+// Check for E3 (parent with more than 2 children)
+for (const count of Object.values(parentsCounts)){
+  if (count > 2) {
+    return 'E3';
+  }
+}
+
+
+
+
